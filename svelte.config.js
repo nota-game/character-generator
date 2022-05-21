4424bfbaeabeff448eba0de3a7055d1844415344
Patrick Kranz <patrick-kranz@live.de>
2022-05-21 22:19:25 +0200

    …

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
-	kit: { 
+	kit: {
 		// trailingSlash: 'always',
 		adapter: adapter({
 			// default options are shown
@@ -17,7 +19,9 @@ const config = {
 			fallback: null,
 			precompress: false
 		}),
-
+		paths: {
+			base: dev ? '' : '/your-repo-name',
+		},
 		prerender: {
 			// This can be false if you're using a fallback (i.e. SPA mode)
 			default: true
