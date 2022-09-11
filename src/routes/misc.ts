58c9539b3a5039b0f76ae515d22bfa7cebe2c05a
Patrick Kranz <patrick-kranz@live.de>
2022-09-11 14:08:21 +0200

    …

--- src/routes/misc.ts
+++ src/routes/misc.ts
@@ -75,7 +75,7 @@ export function getTextTalent(p: TalentDefinition_talent | undefined, format: 'N
         probe.push('MU');
     }
     for (let i = 0; i < (p.Probe.Präzision?.length ?? 0); i++) {
-        probe.push('PR');
+        probe.push('FM');
     }
     for (let i = 0; i < (p.Probe.Stärke?.length ?? 0); i++) {
         probe.push('ST');
