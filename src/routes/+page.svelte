c19c745a87882520eccde1da2022837cbdbedbaf
Patrick Kranz <patrick-kranz@live.de>
2023-01-12 20:54:19 +0100

    …

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,8 +30,8 @@
 	$: lebensabschnitteStore = char?.lebensAbschnitteStore;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Nota Char-gen</h1>
+<p>Alles gelöscht und neu angefange😱</p>
 
 <hr />
 
